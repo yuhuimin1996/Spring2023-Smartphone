4956175312bfb501305c910c4bb4f759a0b04720
Huimin Yu <huiminyu51@gmail.com>
2023-01-16 22:21:48 -0800

    Change the label sentence (Hello World)

--- Hello World/Hello World/ViewController.swift
+++ Hello World/Hello World/ViewController.swift
@@ -20,7 +20,7 @@ class ViewController: UIViewController {
 
     @IBAction func pressMeAction(_ sender: UIButton) {
         print("Button was pressed")
-        lblExample.text = "Button was pressed"
+        lblExample.text = "Hello World"
     }
 }
 
